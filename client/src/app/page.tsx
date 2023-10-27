3fb423f58afecb89f24e1ed1227a8550e3b78224
ItsRahul007 <rg890458@gmail.com>
2023-10-27 22:03:39 +0530

    problem solve. now completing the page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import style from "@/app/style/style.module.css";
 import Link from "next/link";
 import Image from "next/image";
@@ -9,14 +11,17 @@ const roboto = Roboto({
   subsets: ['latin'],
   style: ["normal"]
 });
+import { useRef, useEffect } from "react";
 
 export default function Home() {
-  const { firstSection, secondSection, navBg, rightSpan, linkHover, hero, border, shopNowHover } = style;
+  const { firstSection, secondSection, navBg, rightSpan, linkHover, hero, border, shopNowHover, product } = style;
+  const mainRef = useRef<HTMLDivElement>(null);
+
 
 
   return (
-    <div className="relative flex flex-col justify-center items-center overflow-y-scroll snap-y" id="main">
-      {/* <section className={firstSection}>
+    <>
+      <section className={firstSection} ref={mainRef}>
         <nav className={`h-28 p-3 w-full flex items-center justify-between ${navBg}`}>
           <span className={`ml-6 flex gap-7 text-sm text-white ${linkHover}`}>
             <Link href="/">
@@ -54,56 +59,51 @@ export default function Home() {
           </span>
         </div>
 
-        <div className="h-screen bg-white"></div>
+        <div className={"h-screen w-full bg-white flex items-center justify-center gap-5"}>
+          <div className={"w-96 h-2/3 relative " + product}>
+            <span>
+              <div>
+                <h3 className={"text-3xl " + roboto.className}>
+                  20% Off On Tank Tops
+                </h3>
+                <p className="text-sm leading-7 mt-2">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac dictum.
+                </p>
+                <button className={`mt-5 text-black bg-white p-3 mr-4 font-semibold w-36 ${shopNowHover}`}>SHOP NOW</button>
+              </div>
+            </span>
+          </div>
+          <div className={"w-96 h-2/3 relative " + product}>
+            <span>
+              <div>
+                <h3 className={"text-3xl " + roboto.className}>
+                  Latest Eyewear For You
+                </h3>
+                <p className="text-sm leading-7 mt-2">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac dictum.
+                </p>
+                <button className={`mt-5 text-black bg-white p-3 mr-4 font-semibold w-36 ${shopNowHover}`}>SHOP NOW</button>
+              </div>
+            </span>
+          </div>
+          <div className={"w-96 h-2/3 relative " + product}>
+            <span>
+              <div>
+                <h3 className={"text-3xl " + roboto.className}>
+                  Let&apos;s Lorem Suit Up!
+                </h3>
+                <p className="text-sm leading-7 mt-2">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac dictum.
+                </p>
+                <button className={`mt-5 text-black bg-white p-3 mr-4 font-semibold w-36 ${shopNowHover}`}>SHOP NOW</button>
+              </div>
+            </span>
+          </div>
+        </div>
+
       </section>
 
-      <section className={secondSection}>
-
-      </section> */}
-
-      <div className={firstSection}></div>
-      <div className="absolute top-0 h-screen w-screen overflow-y-scroll">
-        <nav className={`h-28 p-3 w-full flex items-center justify-between ${navBg}`}>
-          <span className={`ml-6 flex gap-7 text-sm text-white ${linkHover}`}>
-            <Link href="/">
-              <Image
-                src={logo}
-                height={90}
-                width={140}
-                alt="Website logo"
-              />
-            </Link>
-            <span className="h-full flex gap-6 font-semibold mt-2">
-              <Link href="/">EVERYTHING</Link>
-              <Link href="/">WOMEN</Link>
-              <Link href="/">MEN</Link>
-              <Link href="/">ACCESSORIES</Link>
-            </span>
-          </span>
-
-          <span className={`flex justify-center items-center mr-5 text-white ${rightSpan} ${linkHover}`}>
-            <Link href="/">ABOUT</Link>
-            <Link href="/">CONTACT US</Link>
-            <Link href="/"><i className="ri-shopping-bag-3-fill"></i></Link>
-            <Link href="/"><i className="ri-user-3-fill"></i></Link>
-          </span>
-        </nav>
-
-        <div className={`${hero} ${roboto.className}`}>
-          <span>
-            <h1 className="text-6xl tracking-wide leading-tight">Raining Offers For <br />Hot Summer!</h1>
-            <h4 className="text-2xl mt-7">25% Off On All Products</h4>
-            <span>
-              <button className={`mt-8 text-black bg-white p-3 mr-4 font-medium ${shopNowHover}`}>SHOP NOW</button>
-              <button className={`p-3 font-medium ${border}`}>FIND MORE</button>
-            </span>
-          </span>
-        </div>
-
-        <div className="h-screen bg-white"></div>
-      </div>
-
-        <div className={secondSection}></div>
-    </div>
+      <section className={secondSection}></section>
+    </>
   );
 };
