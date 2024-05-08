7226eb4b7dd68dec655f5b4206445f65b5dcc887
rahulMile9 <rahul@mile9.io>
2024-05-08 12:45:55 +0530

    CLIENT: fixed some styles

--- src/components/landing page/best-sales/BestSales.tsx
+++ src/components/landing page/best-sales/BestSales.tsx
@@ -3,9 +3,11 @@ import BestSalesSingleItem from './BestSalesSingleItem';
 
 const BestSales = () => {
     return (
-        <section className='w-screen max-w-[1540px] my-10 mx-auto h-auto min-h-[28rem] py-7 px-3 md:py-10 md:px-5 space-y-5 font-rubik' id='best-sales'>
+        <section className='w-screen h-auto max-w-[1540px] my-10 mx-auto min-h-[28rem] py-7 px-3 md:py-10 md:px-5 space-y-5 font-rubik' id='best-sales'>
             <div className='h-20 w-fit mx-auto capitalize font-semibold text-4xl'>best sales</div>
-            <div className='h-auto w-full md:4/5 xl:w-3/5 mx-auto grid md:grid-cols-3 grid-cols-2 md:grid-rows-4 grid-rows-6 gap-3'>
+            <div
+                className='h-auto w-full xl:grid-cols-4 mx-auto grid md:grid-cols-3 grid-cols-2 grid-rows-[auto] gap-3'
+            >
                 <BestSalesSingleItem />
                 <BestSalesSingleItem />
                 <BestSalesSingleItem />
