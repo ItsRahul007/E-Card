bb20a7951bb1f5d80ac5bf346b8e05c790caa9bc
Rahul Ghosh <rahul@mile9.io>
2024-07-12 22:06:30 +0530

    CLIENT: tailwind darkmode enabled

--- src/app/(client)/seller/dashboard/layout.tsx
+++ src/app/(client)/seller/dashboard/layout.tsx
@@ -10,6 +10,7 @@ import { T_JwtVerifyDataType } from "@/lib/types/authToken-type";
 import connectWithMongo from "@/lib/mongoConnection/mongoConnect";
 import User from "@/lib/model/usersSchema";
 import SideNavBar from "@/components/all-products/SideNavBar";
+import Provider from "@/lib/util/Provider";
 
 export const metadata: Metadata = {
   title: "E-Card - Dashboard",
@@ -33,44 +34,46 @@ const layout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="min-h-screen h-auto w-screen max-w-[1540px] mx-auto bg-lightBg font-poppins md:p-4">
       {/* sile options */}
-      <div className="h-full w-full flex flex-col">
-        <nav className="w-full h-16 flex justify-between items-center bg-rootBg shadow-sm px-3 md:rounded-t md:rounded-r">
-          <div className="flex items-center w-auto gap-5 md:gap-32 text-lg md:text-2xl text-rootColor">
-            {/* logo */}
-            <div className="hidden md:flex gap-2 h-full w-40">
-              <Image
-                src="/images/logo.png"
-                alt="logo"
-                height={40}
-                width={40}
-                className="object-contain bg-rootBg"
-              />
-              <div className="text-emerald-500 font-bold font-ubuntu">
-                E-Card
+      <Provider>
+        <div className="h-full w-full flex flex-col">
+          <nav className="w-full h-16 flex justify-between items-center bg-rootBg shadow-sm px-3 md:rounded-t md:rounded-r">
+            <div className="flex items-center w-auto gap-5 md:gap-32 text-lg md:text-2xl text-rootColor">
+              {/* logo */}
+              <div className="hidden md:flex gap-2 h-full w-40">
+                <Image
+                  src="/images/logo.png"
+                  alt="logo"
+                  height={40}
+                  width={40}
+                  className="object-contain bg-rootBg"
+                />
+                <div className="text-emerald-500 font-bold font-ubuntu">
+                  E-Card
+                </div>
               </div>
+              <div className="md:hidden">
+                <SideNavBar searchBarFalse dashboardNav noSearchKeys />
+              </div>
+              <span className="font-bold select-none">
+                <span className="opacity-50 font-medium mr-1">Hello,</span>
+                <span>{decodedToken.user.name}!</span>
+              </span>
             </div>
-            <div className="md:hidden">
-              <SideNavBar searchBarFalse dashboardNav noSearchKeys />
+            <div>
+              <UserProfile userAvatar={userAvatar} />
             </div>
-            <span className="font-bold select-none">
-              <span className="opacity-50 font-medium mr-1">Hello,</span>
-              <span>{decodedToken.user.name}!</span>
-            </span>
-          </div>
-          <div>
-            <UserProfile userAvatar={userAvatar} />
-          </div>
-        </nav>
-        <div className="flex gap-3 flex-1">
-          <div className="min-h-full xl:w-72 w-64 bg-rootBg md:flex hidden flex-col gap-2 px-3 py-5 shadow-md rounded-b">
-            <DashboardMenus />
-          </div>
+          </nav>
+          <div className="flex gap-3 flex-1">
+            <div className="min-h-full xl:w-72 w-64 bg-rootBg md:flex hidden flex-col gap-2 px-3 py-5 shadow-md rounded-b">
+              <DashboardMenus />
+            </div>
 
-          <div className="flex-1 pt-2 h-full max-w-[100vw] overflow-hidden min-h-[84vh]">
-            {children}
+            <div className="flex-1 pt-2 h-full max-w-[100vw] overflow-hidden min-h-[84vh]">
+              {children}
+            </div>
           </div>
         </div>
-      </div>
+      </Provider>
     </main>
   );
 };
--- src/components/seller/dashboard/UserProfileDropdown.tsx
+++ src/components/seller/dashboard/UserProfileDropdown.tsx
@@ -30,12 +30,14 @@ const UserProfile: React.FC<I_UserProfile> = ({ userAvatar }) => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-lightBg text-rootColor py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white dark:bg-zinc-900 text-rootColor py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           {userNavigation.map((item) => (
             <Menu.Item key={item.href + "user-profile-dropdown"}>
               <Link
                 href={item.href}
-                className={"hover:bg-rootBg block px-4 py-2 text-sm"}
+                className={
+                  "hover:bg-zinc-100 dark:hover:bg-zinc-800 block px-4 py-2 text-sm"
+                }
               >
                 {item.name}
               </Link>
